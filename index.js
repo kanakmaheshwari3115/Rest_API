a9c2acc52a8a6359837a569c2987a4c8d7154491
kanakmaheshwari3115 <mahe.kanak@gmail.com>
2024-07-27 23:53:41 +0530

    updates

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.route('/api/users/:id')
 app.post('/api/users', (req, res)=>{
     const body = req.body;
     if (!body || !body.first_name ||  !body.last_name ||  !body.email ||  !body.gender ||  !body.job_title){
-        res.status(400).json({msg : "All fields are required..."})
+        return res.status(400).json({msg : "All fields are required..."})
     }
     users.push({id : users.length + 1, ...body});
     fs.writeFile('MOCK_DATA.json', JSON.stringify(users), (err, data)=>{
